db545cc97b5a6cfd181888e47a004d5c1566bd87
Deekshitha Thumma <DThumma@zoll.com>
2021-01-25 15:21:04 -0700

    Implemented Publish and Send Command for Banking Microservice

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -9,6 +9,9 @@ using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Data.Respository;
 using MicroRabbit.Banking.Data.Context;
+using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.CommandHandlers;
 
 namespace MicroRabbit.Infra.IoC
 {
@@ -18,6 +21,9 @@ namespace MicroRabbit.Infra.IoC
 		{
 			//Domain Bus
 			services.AddTransient<IEventBus, RabbitMQBus>();
+
+			//Domain Commands
+			services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
 			//Application Services
 			services.AddTransient<IAccountService,AccountService>();
